97566ed3ce3632761808425c251dfb661e347eff
Unknown <waynene0831@hotmail.com>
2018-02-05 17:55:54 +0800

    0205

--- module.js
+++ module.js
@@ -194,7 +194,8 @@ class Module {
 
 
 	init () {
-			$('.box').on('click',function(){
+
+			this.$ele.find('.box').on('click',function(){
     		$('td .box').removeClass('active');
     		$('td .box').removeClass('HLgray');
     		var t = $(this).index();
@@ -202,51 +203,40 @@ class Module {
             $(this).children('.box:eq(' + t + ')').addClass('HLgray')});
             $(this).addClass('active');
             $(this).siblings().addClass('HLgray');
+            console.log(this);
     		;})
 
-           //-----------------------------------第二個
-           $('.secBox').on('click',function(){
-        	//--清除原本的特效
+          this.$ele.find('.secBox').on('click',function(){
+          	console.log(this);
         	$('.arrive').find('.plane').removeClass('planeblock'); 
         	$('.sectd1').find('.plane').removeClass('planeblock'); 
     		$('td .secBox').removeClass('active');
     		$('.secgray').removeClass('active');
-    		$('.setoffbox').removeClass('active');
-
-    		//--hightlight特效
+    		$('.setoffbox').removeClass('active');  
     		$(this).addClass('active');
     		var s = $(this).index();
-
-            //--飛機 目的地
-            $(this).addClass('active');
             $('.arrive').children('.secgray:eq(' + s + ') ').addClass('active'); 
             $('.arrive').children('.arrivebox:eq(' + s + ')').find('.plane').addClass('planeblock'); 
-            //--飛機 出發地
             var sa =$(this).parents('.seclongbox').index()-1;
-            console.log(sa); 
             $('.sectd1').children('.seclgray:eq(' +sa+ ')').addClass('active');
-            $('.sectd1').children('.seclgray:eq(' +sa+ ')').find('.plane').addClass('planeblock');
-
+            $('.sectd1').children('.seclgray:eq(' +sa+ ')').find('.plane').addClass('planeblock');     
     		;})
- 
+
+      
 
 
 
-
-
-
-
+       //--
 		var move = this.option.count.slide;
 		var show =this.option.count.show;
+		var speed = this.option.speed;
         // console.log(this);
         // console.log(this.ele);
         // console.log(this.$ele);
 
+        // console.log(speed);
         // console.log(show);
-        // console.log(move);
-       
-
-
+        // console.log(move);      
 
         var slide=0;
 	    var sliderWidth=90;
@@ -254,39 +244,34 @@ class Module {
         var slidershowWdith = 270;
         var width;
         var slidemove = slidershowWdith/show;
-
-        // console.log(slidemove);
-
         var boxsize = slidershowWdith/show+'px';
-
+           
         	if (window.matchMedia("(max-width: 376px)").matches){
             this.$ele.find('.box').css('min-width', boxsize);
             this.$ele.find('.gray').css('min-width', boxsize);
         	}
     //-----------------------
 	    function goSlider(slide){
-
 	      width=0-slidemove*slide+"px";
            $(".slideTd").css("left", width);
-          if(slide<=slideCount-show){
-	      $("#dotwrap1 .dot1").removeClass('active');
-	      $("#dotwrap1 .dot1").eq(slide).addClass('active');}
+           $(".slideTd").css('transition', speed+'s' );
+           if(slide<=slideCount-show){
+	       $("#dotwrap1 .dot1").removeClass('active');
+	       $("#dotwrap1 .dot1").eq(slide).addClass('active');}
 	    }
-        
+         
 
-           //slide<5
 	  this.$ele.find('.goRight').on('click',function(){
 	   	if (window.matchMedia("(max-width: 376px)").matches){
 	   		if(slide+move<=slideCount-show){
 	        slide=slide+move;
 	        goSlider(slide);
-	        console.log(slide);
 	      }
           else{
             slide=slideCount-show;
-            goSlider(slide);
+            goSlider(slide); 
            }
-	  }
+	    }
 	    });
        
         this.$ele.find('.goLeft').on('click',function(){
@@ -299,14 +284,9 @@ class Module {
             goSlider(slide);
 	      }
 	     }
-
 	    });
 
-
  
-/////////
-
-       
 
         var secslide=0;
 	    var secsliderWidth=110;
@@ -314,7 +294,6 @@ class Module {
 	    var secslidershowWdith = 220;
         var secwidth;
         var secslidemove = secslidershowWdith/show;
-
         var secboxsize = secslidershowWdith/show+'px';
 
         if (window.matchMedia("(max-width: 376px)").matches){
@@ -322,14 +301,12 @@ class Module {
         	}
 
         function secgoSlider(secslide){
-          //滑動特效
 	      secwidth=0-secslidemove*secslide+"px";
 	      $(".secslideTd").css("left", secwidth);
-	      //點點特效
 	      if(slide<=slideCount-show){
 	      $("#dotwrap2 .dot2").removeClass('active');
 	      $("#dotwrap2 .dot2").eq(secslide).addClass('active');}
-	    }
+	      }
 
 
 	  this.$ele.find('.secgoRight').on('click',function(){
@@ -353,12 +330,8 @@ class Module {
             slide=0;
             secgoSlider(secslide);
 	      }
-
-	  }
-	    });
-
-
-    	
+	     }
+	    }); 	
 			return this;
 			}
 
